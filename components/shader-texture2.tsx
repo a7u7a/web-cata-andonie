6eade153f5f0dc1668042697860fe5351b77b2f8
a7u7a <esteban@esrs.co>
2022-09-07 17:30:40 -0400

    testing needsupdate fix

--- components/shader-texture2.tsx
+++ components/shader-texture2.tsx
@@ -43,7 +43,7 @@ const QuadTest = () => {
   useFrame((state) => {
     if (matRef.current.uniforms) {
       matRef.current.uniforms.u_time.value = state.clock.elapsedTime;
-      matRef.current.needsUpdate = true;
+      matRef.current.uniformsNeedUpdate = true;
     }
   });
 
