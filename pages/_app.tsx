e6afa08154c171ec9fcdef40b036d91c8c304d75
a7u7a <esteban@esrs.co>
2022-11-09 17:10:34 -0300

    hewo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { useRouter } from "next/router";
+import { useState, forwardRef, useEffect } from "react";
+
+interface TestWrapperProps {
+  children: JSX.Element;
 }
 
-export default MyApp
+const TestWrapper = ({ children }: TestWrapperProps) => {
+  const { route } = useRouter();
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log("TestWrapper route", route);
+    setCounter(counter + 1);
+  }, [route]);
+
+  useEffect(() => {
+    console.log("counter", counter);
+  }, [counter]);
+
+  return <div>{children}</div>;
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <TestWrapper>
+      <Component {...pageProps} />
+    </TestWrapper>
+  );
+}
+
+export default MyApp;
